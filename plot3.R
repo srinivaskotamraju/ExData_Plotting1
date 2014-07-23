a764d62a64edbe64a0477586de0ae59ee8866f0d
srinivaskotamraju <kotamraju.srinivas@gmail.com>
2014-07-23 14:06:52 +0530

    used lines instead of points

--- plot3.R
+++ plot3.R
@@ -22,8 +22,8 @@
 	
 	with(data,{
 		plot(data$Time,data$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering", col = "black")
-		points(data$Time,data$Sub_metering_2,type = "l", col = "red")
-		points(data$Time,data$Sub_metering_3,type = "l", col = "blue")
+		lines(data$Time,data$Sub_metering_2,col = "red")
+		lines(data$Time,data$Sub_metering_3,col = "blue")
 		legend("topright" , 
         c("Sub_metering_1 ","Sub_metering_2","Sub_metering_3"), 
         lty=1, 
--- plot4.R
+++ plot4.R
@@ -26,8 +26,8 @@
 		plot(Time,Global_active_power,type="l",xlab="",ylab="Global Active Power")
 		plot(Time,Voltage,type="l",xlab="datetime",ylab="Voltage")
 		plot(data$Time,data$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering", col = "black")
-		points(data$Time,data$Sub_metering_2,type = "l", col = "red")
-		points(data$Time,data$Sub_metering_3,type = "l", col = "blue")
+		lines(data$Time,data$Sub_metering_2,col = "red")
+		lines(data$Time,data$Sub_metering_3,col = "blue")
 		legend("topright",c("Sub_metering_1 ","Sub_metering_2","Sub_metering_3"), 
         lty=1,bty="n",col=c("black", "red","blue")) 
 		plot(Time,Global_reactive_power,type="l",xlab="datetime")
